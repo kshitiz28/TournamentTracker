9895796252541b13e818553f59a1d4b65afb7672
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-07 15:36:21 +0545

    Dapper SQL connection done for createprizes

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+
+
+//@PlaceNumber int,
+//@PlaceName nvarchar(50),
+//@PrizeAmount money,
+//@PrizePercentage float,
+//@id int = 0 output 
 
 namespace TrackerLibrary.DataAccess
 {
@@ -19,9 +28,22 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information,including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
+            {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
 
-            return model;
+                connection.Execute("dbo.spPrizes_Insert",p,commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+
+                return model;
+
+            }
         }
     }
 }
