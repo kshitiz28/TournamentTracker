e4a7eef1ad3ea54fa30e586145d9f22c925e3205
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-08 14:36:35 +0545

    Config and connector process class made for text connection.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -13,9 +13,12 @@ namespace TrackerLibrary.DataAccess
         //TODO - Wire up the CreatePrize for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
-
-            return model;
+            // Load the text file
+            //Convert tge text to List<PrizeModel>
+            //Find the max ID
+            //Add the new record with the new ID (max+1)
+            //Convert the prizes to List<string>
+            //Save the List<string> to the text file
         }
     }
 }
---
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.DataAccess.TextConnector
+{
+    public static class TextConnectorProcessor
+    {
+        public static string FullFilePath(string fileName) //PrizeModels.csv
+        {
+            // D:\Learning Start\data\TournamentTracker\PrizeModels.csv
+            return $"{ConfigurationManager.AppSettings["filePath"]}\\{ fileName }";
+        }
+    }
+}
