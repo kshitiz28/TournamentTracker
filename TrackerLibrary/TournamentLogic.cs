110aa23c799ac2472880c070ca76cd7e8f3ed458
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-13 13:27:54 +0545

    First Round matchup logic implemented.

--- TrackerLibrary/TournamentLogic.cs
+++ TrackerLibrary/TournamentLogic.cs
@@ -23,6 +23,30 @@ namespace TrackerLibrary
             int byes = NumberOfByes(rounds,randomizedTeams.Count);
         }
 
+
+        private static List<MatchupModel> CreateFirstRound(int byes, List<TeamModel> teams)
+        {
+            List<MatchupModel> output = new List<MatchupModel>();
+            MatchupModel curr = new MatchupModel();
+
+            foreach (TeamModel team in teams) 
+            {
+                curr.Entries.Add(new MatchupEntryModel { TeamCompeting = team });
+                if(byes > 0 || curr.Entries.Count > 1)
+                {
+                    curr.MatchupRound = 1;
+                    output.Add(curr);
+                    curr = new MatchupModel();
+                    
+                    if(byes > 0)
+                    {
+                        byes -= 1;
+                    }
+                }
+            }
+
+            return output;
+        }
         private static int NumberOfByes(int rounds, int numberOfTeams)
         {
             int output = 0;
