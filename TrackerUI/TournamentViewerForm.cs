aef7242a7fea8b3f8e40330df4c842d0be10b830
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-20 21:02:52 +0545

    Round load dropdown made.

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -6,6 +6,7 @@ namespace TrackerUI
     {
 
         private TournamentModel tournament;
+        List<int> rounds = new List<int>();
         public TournamentViewerForm(TournamentModel tournamentModel)
         {
             InitializeComponent();
@@ -13,11 +14,38 @@ namespace TrackerUI
             tournament= tournamentModel;
 
             LoadFormData();
+
+            LoadRounds();
         }
 
         private void LoadFormData()
         {
             tournamentName.Text = tournament.TournamentName;
+        }
+
+        private void WireUpLists()
+        {
+            roundDropDown.DataSource = null;
+            roundDropDown.DataSource = rounds;
+        }
+
+        private void LoadRounds()
+        {
+            rounds = new List<int>();
+
+            rounds.Add(1);
+            int currRound = 1;
+
+            foreach (List<MatchupModel> matchups in tournament.Rounds)
+            {
+                if(matchups.First().MatchupRound > currRound)
+                {
+                    currRound = matchups.First().MatchupRound;
+                    rounds.Add(currRound);
+                }
+
+            }
+            WireUpLists();
         }
 
         private void TournamentViewerForm_Load(object sender, EventArgs e)
