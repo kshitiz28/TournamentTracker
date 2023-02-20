ae184fd27967028ba6d2de21dd3fc4a457d2017b
kshitiz <kshitiz.dhakal50@gmail.com>
2023-02-20 20:49:03 +0545

    Load Tournament and Tournament Viewer form wired.

--- TrackerUI/TournamentDashboardForm.cs
+++ TrackerUI/TournamentDashboardForm.cs
@@ -36,5 +36,12 @@ namespace TrackerUI
             CreateTournamentForm frm = new CreateTournamentForm();
             frm.Show();
         }
+
+        private void loadTournamentButton_Click(object sender, EventArgs e)
+        {
+            TournamentModel tm = (TournamentModel)loadExistingTournamentDropDown.SelectedItem;
+            TournamentViewerForm frm = new TournamentViewerForm(tm);
+            frm.Show();
+        }
     }
 }
--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -1,10 +1,23 @@
+using TrackerLibrary.Models;
+
 namespace TrackerUI
 {
     public partial class TournamentViewerForm : Form
     {
-        public TournamentViewerForm()
+
+        private TournamentModel tournament;
+        public TournamentViewerForm(TournamentModel tournamentModel)
         {
             InitializeComponent();
+
+            tournament= tournamentModel;
+
+            LoadFormData();
+        }
+
+        private void LoadFormData()
+        {
+            tournamentName.Text = tournament.TournamentName;
         }
 
         private void TournamentViewerForm_Load(object sender, EventArgs e)
